f5cb8a86e473327d0d73437825de6250dd691e0c
PASUPATHI <pasupathy7274@gmail.com>
2018-02-23 15:06:03 +0530

    Add files via upload

---
+++ beginner/even or odd.py
@@ -0,0 +1,20 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Administrator
+#
+# Created:     23-02-2018
+# Copyright:   (c) Administrator 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+num =int(input("Enter a number: "))
+
+if 1<=num<=100000 and (num % 2) == 0:
+   print("it is Even")
+elif 1<=num<=100000:
+   print("it is Odd")
+else:
+    print("limit exceeded")
