99d22dd0ec2e5c6ba9d3d258ea2d9e9206c7531a
PASUPATHI <pasupathy7274@gmail.com>
2018-02-23 16:22:31 +0530

    Add files via upload

---
+++ beginner/PRINT HELLO.py
@@ -0,0 +1,13 @@
+#-------------------------------------------------------------------------------
+# Name:        PRINT HELLO
+# Purpose:
+#
+# Author: bb
+# Created:     23-02-2018
+# Copyright:   (c) Administrator 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+n=input("enter the times")
+a= "hello"
+for a in range(0,n):
+   print("Hello")
