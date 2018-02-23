4b8a435c6625126cbe803f5f76f49ea09119b852
PASUPATHI <pasupathy7274@gmail.com>
2018-02-23 15:38:25 +0530

    Add files via upload

---
+++ beginner/ALPHABET OR NOT.py
@@ -0,0 +1,15 @@
+#-------------------------------------------------------------------------------
+# Name:        ALPHABET OR NOT
+# Purpose:
+#
+# Author: bb
+# Created:     23-02-2018
+# Copyright:   (c) Administrator 2018
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+value=raw_input("enter the letter")
+if 'a'<=value<='z' or 'A'<=value<='Z':
+  print("alphabet")
+else:
+  print("no")
